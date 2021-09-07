c4905fe7826bc2c1aae56e6069bad99545aa6789
NicoRenz0x <nicolasdirenzo@hotmail.com.ar>
2021-09-07 16:46:09 -0300

    Agregando estilos p1

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
 app.listen(3000, () => {
-    console.log("Servidor iniciado en https://localhost:3000")
+    console.log("Servidor iniciado en http://localhost:3000/")
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
 
-const publicPath = path.resolve(__dirname, "./public");
-app.use( express.static(publicPath) );
