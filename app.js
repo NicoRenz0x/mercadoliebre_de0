9df48bd4aeb1571b5e28a4e8009a8c5e04cb1420
NicoRenz0x <nicolasdirenzo@hotmail.com.ar>
2021-09-14 16:25:15 -0300

    register HTML + CSS

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
-})
+});
 
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
-})
+});
 
