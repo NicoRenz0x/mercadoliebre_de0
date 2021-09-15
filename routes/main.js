d1ddd5834bcfca57876c4bc06daa5b20169c4c85
NicoRenz0x <nicolasdirenzo@hotmail.com.ar>
2021-09-15 15:20:25 -0300

    Avance con el routing del MVC

--- routes/main.js
+++ routes/main.js
@@ -8,4 +8,12 @@ router.get('/', homeController.home)
 router.get('/register', registerController.home)
 router.get('/login', loginController.home)
 
+router.get('/', function (req, res){
+
+})
+
+router.get('/detalle/:id', function (req, res){
+    
+})
+
 module.exports = router
