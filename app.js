bae83cb29ba49409b1a6739f3136e55528340fd5
NicoRenz0x <nicolasdirenzo@hotmail.com.ar>
2021-09-14 22:36:50 -0300

    Avance 4

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+/*app.listen(3000, () => {
+    console.log("Servidor iniciado en http://localhost:3000/")
+});*/
+app.listen(process.env.PORT || 3000,function(){
     console.log("Servidor iniciado en http://localhost:3000/")
 });
 
@@ -13,10 +15,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
 
-app.get('/register', (req, res) => {
+app.get('/register.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 });
 
-app.get('/login', (req, res) => {
+app.get('/login.html', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'))
 });
